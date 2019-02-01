8d6cd4882d45d1672e61bd4d474e4f72ac22f7f2
Andrew <andrew.liu01@libertymutual.com>
2019-02-01 14:57:19 -0500

    Convert to table, add rank suffixes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {createPlayer, getPlayers, deletePlayer} from './actions';
 
+const rankString = number => {
+  if (number === 1) {
+    return '1st';
+  }
+  if (number === 2) {
+    return '2nd';
+  }
+  if (number === 3) {
+    return '3rd';
+  }
+  if (number === 21) {
+    return '21st';
+  }
+  if (number === 22) {
+    return '22nd';
+  }
+  if (number === 23) {
+    return '23rd';
+  }
+  return `${number}th`;
+}
+
 class App extends React.Component {
   constructor (props) {
     super(props);
@@ -38,26 +60,33 @@ class App extends React.Component {
     return (
       <Fragment>
         {title}
-        <ul className="list-group list-group-flush">
+        <table className="table">
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Actions</th>
+          </tr>
           {
             this.props.players.map(player =>
-              <li className="list-group-item">
-                {player.rank}.&nbsp;
-                {player.name}&nbsp;
-                <button
-                  className="btn btn-sm btn-outline-primary"
-                >
-                  Swap
-                </button>&nbsp;
-                <button
-                  className="btn btn-sm btn-outline-danger"
-                  onClick={this.handleDeleteButtonClick(player.playerId)}
-                >
-                  Delete
-                </button>
-              </li>)
+              <tr>
+                <td>{rankString(player.rank)}</td>
+                <td>{player.name}</td>
+                <td>
+                  <button
+                    className="btn btn-sm btn-outline-primary"
+                  >
+                    Swap
+                  </button>&nbsp;
+                  <button
+                    className="btn btn-sm btn-outline-danger"
+                    onClick={this.handleDeleteButtonClick(player.playerId)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>)
           }
-        </ul>
+        </table>
         <button
           type='button'
           className="btn btn-outline-secondary"
