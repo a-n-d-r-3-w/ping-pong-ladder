7f07b67e06f820b3b30ac20595d091752463f48e
Andrew <andrew.liu01@libertymutual.com>
2019-03-01 15:24:48 -0500

    Modify DB name to match mLab

--- server/connectRunClose.js
+++ server/connectRunClose.js
@@ -1,13 +1,11 @@
 const { MongoClient } = require('mongodb')
 
 const DB_URL = process.env.DB_URL
-const DB_NAME = 'ping-pong-ladder'
+const DB_NAME = 'heroku_wjbc8kh4'
 
 const connectRunClose = async (collectionName, fn) => {
   // noinspection JSCheckFunctionSignatures, because WebStorm doesn't know about useNewUrlParser.
-  console.log("connecting to " + DB_URL)
   const client = await MongoClient.connect(DB_URL, { useNewUrlParser: true })
-  console.log("connected")
   const db = client.db(DB_NAME)
   const collection = db.collection(collectionName)
   const result = await fn(collection)
